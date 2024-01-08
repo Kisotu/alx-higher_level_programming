cf5f1f350100cb09aa974e269f2017bbc80b5785
Oloishorua <jacoboloishorua@gmail.com>
2024-01-08 20:56:21 +0300

    task 7 - script that prints matrix of integers

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -1,6 +1,6 @@
 #!/usr/bin/pytho3
 def print_matrix_integer(matrix=[[]]):
-    for h_vertix in matrix:
-        for v_vertix in h_vertix:
-            print("{:d}".format(v_vertix), end=" " if v_vertix != h_vertix[-1] else "")
+    for row in matrix:
+        for col in row:
+            print("{:d}".format(col), end=" " if col != row[-1] else "")
         print()
