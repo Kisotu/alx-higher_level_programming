ffe59b37730f5c6f5575c4b728a5a8311f6f6e1f
Oloishorua <jacoboloishorua@gmail.com>
2024-01-09 01:22:48 +0300

    error fix, imports

--- 0x03-python-data_structures/13-is_palindrome.c
+++ 0x03-python-data_structures/13-is_palindrome.c
@@ -1,6 +1,28 @@
 #include "lists.h"
 
 /**
+ * reverse_listint - func to reverse a linked list
+ * @head: pointer to first node of list
+ * Return: pojnter to first node
+ */
+void reverse_listint(listint_t **head)
+{
+        listint_t *curr = *head;
+        listint_t *prev = NULL;
+        listint_t *next = NULL;
+
+        while (curr)
+        {
+                next = curr->next;
+                curr->next = prev;
+                prev = curr;
+                curr = next;
+        }
+
+        *head = prev;
+}
+
+/**
  * is_palindrome - func to check if a list is a palindrome
  * @head: pointer to first node
  * Return: 1 if palindroe, 0 if not
@@ -44,26 +66,4 @@ int is_palindrome(listint_t **head)
 		return (1);
 
 	return (0);
-}
-
-/**
- * reverse_listint - func to reverse a linked list
- * @head: pointer to first node of list
- * Return: pojnter to first node
- */
-void reverse_listint(listint_t **head)
-{
-	listint_t *curr = *head;
-	listint_t *prev = NULL;
-	listint_t *next = NULL;
-
-	while (curr)
-	{
-		next = curr->next;
-		curr->next = prev;
-		prev = curr;
-		curr = next;
-	}
-
-	*head = prev;
 }
