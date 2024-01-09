8a33f0459a10c1791daf79e443f48c76967132fe
Oloishorua <jacoboloishorua@gmail.com>
2024-01-09 20:59:04 +0300

    task 15 - C function that prints basic info about Python lists

--- 0x03-python-data_structures/100-print_python_list_info.c
+++ 0x03-python-data_structures/100-print_python_list_info.c
@@ -1,6 +1,6 @@
-#include <object.h>
-#include <Python.h>
+#include <python.h>
 #include <listobject.h>
+#include <object.h>
 
 /**
  * print_python_list_info - func that prints basic info abt python lists
