fd3231b0cfdf7e5f63ecc89aeed54e7e7d6f2755
Oloishorua <jacoboloishorua@gmail.com>
2024-01-11 01:19:43 +0300

    task 11 - script that multiplies values by a number

--- 0x04-python-more_data_structures/11-multiply_list_map.py
+++ 0x04-python-more_data_structures/11-multiply_list_map.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
 def multiply_list_map(my_list=[], number=0):
     '''function that returns a list with values multiplied by a number'''
-    return (list(map((lambda k: k * number), my_list)))
+    return (list(map((lambda i: i * number), my_list)))
