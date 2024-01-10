d0433b287f977cce28ecbb9bf0ca61f3bc5dda5a
Oloishorua <jacoboloishorua@gmail.com>
2024-01-11 01:45:17 +0300

    task 102 - script that deletes a key that has certain value

---
+++ 0x04-python-more_data_structures/102-complex_delete.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python3
+def complex_delete(a_dictionary, value):
+    for dict_key in list(a_dictionary):
+        if a_dictionary[dict_key] == value:
+            del a_dictionary[dict_key]
+        return a_dictionary
