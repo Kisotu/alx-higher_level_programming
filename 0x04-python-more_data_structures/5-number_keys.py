b2c0d9a0b2d725d1582c3c86f662d67483ac68e1
Oloishorua <jacoboloishorua@gmail.com>
2024-01-11 00:43:03 +0300

    error fix

--- 0x04-python-more_data_structures/5-number_keys.py
+++ 0x04-python-more_data_structures/5-number_keys.py
@@ -1,9 +1,3 @@
 #!/usr/bin/python3
 def number_keys(a_dictionary):
-    numb = 0
-    dict_keys = list(a.dictionary.keys())
-
-    for k in dict_keys:
-        numb += k
-
-    return numb
+    return len(a_dictionary.keys())
