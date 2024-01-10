722f9107347f3cf2409447909374b97681cd8bcc
Oloishorua <jacoboloishorua@gmail.com>
2024-01-11 01:47:48 +0300

    task 102 - correction

--- 0x04-python-more_data_structures/102-complex_delete.py
+++ 0x04-python-more_data_structures/102-complex_delete.py
@@ -3,4 +3,4 @@ def complex_delete(a_dictionary, value):
     for dict_key in list(a_dictionary):
         if a_dictionary[dict_key] == value:
             del a_dictionary[dict_key]
-        return a_dictionary
+    return a_dictionary
