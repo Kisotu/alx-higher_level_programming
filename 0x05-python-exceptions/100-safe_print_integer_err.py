090de1b3ba7e448fe92bf7834374df225be6f0df
Oloishorua <jacoboloishorua@gmail.com>
2024-01-22 23:57:51 +0300

    task 100 -  a function that prints an integer.

--- 0x05-python-exceptions/100-safe_print_integer_err.py
+++ 0x05-python-exceptions/100-safe_print_integer_err.py
@@ -1,9 +1,10 @@
 #!/usr/bin/python3
-
 import sys
+
 
 def safe_print_integer_err(value):
     ''' a function that prints an integer.'''
+
     try:
         print("{:d}".format(value))
         return (True)
