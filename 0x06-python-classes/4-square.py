ef32ed573d6f22b1d30efe4771c75763207c25d6
Oloishorua <jacoboloishorua@gmail.com>
2024-01-24 01:03:11 +0300

    task 4 - Square class with setter method

--- 0x06-python-classes/4-square.py
+++ 0x06-python-classes/4-square.py
@@ -15,7 +15,7 @@ class Square():
     @size.setter
     def size(size, value):
         if (type(value) is not int):
-            raise TypeError("size mnust be an integer")
+            raise TypeError("size must be an integer")
         elif (value < 0):
             raise ValueError("size must be >= 0")
         self.__size = value
