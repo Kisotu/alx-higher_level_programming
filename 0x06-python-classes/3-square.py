f212d26f9b436b70e4e6dc86ac152b5c3b4cb307
Oloishorua <jacoboloishorua@gmail.com>
2024-01-23 23:57:38 +0300

    task 3 - square class with area method

--- 0x06-python-classes/3-square.py
+++ 0x06-python-classes/3-square.py
@@ -8,7 +8,7 @@ class Square():
     def __init__(self, size=0):
         if (type(size) is not int):
             raise TypeError("size must be an integer")
-        elif (sie < 0):
+        elif (size < 0):
             raise ValueError("size must be >= 0")
         self.__size = size
 
