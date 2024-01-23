acbec29d364ae8d122064cafcc16a71dc7533903
Oloishorua <jacoboloishorua@gmail.com>
2024-01-23 22:56:25 +0300

    task 1 - instantiated square class

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+'''task 1 - square class with instantiation'''
+
+
+class Square():
+    '''A square class'''
+
+    def __init__(self, size):
+        '''init method to instantiate class
+
+        Args:
+            size (init): square size
+        '''
+        self.__size = size
