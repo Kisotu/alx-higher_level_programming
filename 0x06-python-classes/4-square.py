5a949cd54ca089c2d42409fffcfc18d81a336f23
Oloishorua <jacoboloishorua@gmail.com>
2024-01-24 01:05:38 +0300

    task 4 - typo fix

--- 0x06-python-classes/4-square.py
+++ 0x06-python-classes/4-square.py
@@ -13,7 +13,7 @@ class Square():
         return self.__size
 
     @size.setter
-    def size(size, value):
+    def size(self, value):
         if (type(value) is not int):
             raise TypeError("size must be an integer")
         elif (value < 0):
