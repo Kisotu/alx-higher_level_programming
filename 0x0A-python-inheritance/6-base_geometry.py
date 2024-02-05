00e0c99ae624d6495dabf220f23a52182b9cacd5
Oloishorua <jacoboloishorua@gmail.com>
2024-02-06 02:27:55 +0300

    Task 6 - improve geometry

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+'''Task 6 - Improve Geometry'''
+
+
+class BaseGeometry:
+    '''A base geometry class'''
+
+    def area(self):
+        '''exception: not implemented'''
+        raise Exception("area() is not implemented")
